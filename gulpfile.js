7a277987fa9f0abee15afd4777412f7b682d7302
Roi <vosroix@gmail.com>
2020-03-16 12:37:13 +0500

    created new build and src folders

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,14 @@ const plumber = require('gulp-plumber');
 const fileinclude = require('gulp-file-include');
 
 gulp.task('html', function(callback) {
-    return gulp.src('./app/html/*.html')
+    return gulp.src('./src/html/*.html')
         .pipe( fileinclude({ prefix: '@@' }) )
-        .pipe( gulp.dest('./app/') )
+        .pipe( gulp.dest('./build/') )
     callback();
 });
 
 gulp.task('scss', function(callback) {
-    return gulp.src('./app/scss/main.scss')
+    return gulp.src('./src/scss/main.scss')
         .pipe( plumber({
             errorHandler: notify.onError(function(err) {
                 return {
@@ -33,29 +33,29 @@ gulp.task('scss', function(callback) {
             overrideBrowserslist: ['last 4 versions']
         }))
         .pipe( sourcemaps.write() )
-        .pipe( gulp.dest('./app/css/') )
+        .pipe( gulp.dest('./build/css/') )
     callback(); 
 });
 
 
 gulp.task('watch', function() {
-    watch('./app/*.html', gulp.parallel( browserSync.reload ));
-    watch('./app/css/**/*.css', gulp.parallel( browserSync.reload ));
-    watch('./app/scss/**/*.scss', function() {
+    watch('./build/*.html', gulp.parallel( browserSync.reload ));
+    watch('./build/css/**/*.css', gulp.parallel( browserSync.reload ));
+    watch('./src/scss/**/*.scss', function() {
         setTimeout(gulp.parallel('scss'), 500);
     });
 
-    watch('./app/html/**/*.html', gulp.parallel('html'));
+    watch('./src/html/**/*.html', gulp.parallel('html'));
 });
 
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "./app/"
+            baseDir: "./build/"
         }
     });
 });  
 
 
 
-gulp.task('default', gulp.parallel('server', 'watch', 'scss', 'html'));
+gulp.task('default', gulp.series( gulp.parallel('scss', 'html'), gulp.parallel('server', 'watch') ));
